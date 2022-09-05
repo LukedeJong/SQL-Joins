871b18c1522399f967197f85cc0e448e1d326ffb
Luke de Jong <ldejong9938@gmail.com>
2022-09-05 09:42:47 +0200

    Opdracht 3 toegevoegd

--- antwoorden-school-app.sql
+++ antwoorden-school-app.sql
@@ -6,3 +6,8 @@ SELECT naam AS voornaam, studie.studie, plaats.plaatsnaam FROM student
 JOIN studie on studie.code = student.studie
 JOIN plaats ON plaats.code = student.woonplaats
 WHERE studie.plaats != "Den Haag";
+
+SELECT naam, vaknaam, periode FROM student
+JOIN deelname ON deelname.id = student.id
+JOIN vak on vak.code = deelname.ak
+ORDER BY naam, periode;
