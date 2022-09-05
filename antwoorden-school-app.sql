8c28aeb0aabe7cb4e2420b1fbb3d0579dfbe172b
Luke de Jong <ldejong9938@gmail.com>
2022-09-05 09:23:37 +0200

    opdracht 2 toegevoegd

--- antwoorden-school-app.sql
+++ antwoorden-school-app.sql
@@ -1,3 +1,8 @@
 SELECT naam, woonplaats FROM student 
 JOIN plaats ON plaats.code = student.woonplaats
 WHERE plaatsnaam != 'Haarlem' AND beurs > 90 ORDER BY plaatsnaam;
+
+SELECT naam AS voornaam, studie.studie, plaats.plaatsnaam FROM student
+JOIN studie on studie.code = student.studie
+JOIN plaats ON plaats.code = student.woonplaats
+WHERE studie.plaats != "Den Haag";
