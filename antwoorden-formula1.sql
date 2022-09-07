176a4ffb6a51637df6fec5ba81ab4158c8ddfdf5
Luke de Jong <ldejong9938@gmail.com>
2022-09-07 09:45:24 +0200

    Opdrach 2 formula1

--- antwoorden-formula1.sql
+++ antwoorden-formula1.sql
@@ -6,3 +6,11 @@ WHERE races.year = 2018
 ORDER BY races.year DESC;
 
 -- Opdracht 2
+
+SELECT races.name, drivers.surname, driver_standing.points FROM races
+JOIN driver_standing ON driver_standing.raceId = races.raceId
+JOIN drivers ON drivers.driverId = driver_standing.driverId
+WHERE driver_standing.points = 10 AND races.year = 2017
+ORDER BY races.name;
+
+-- Opdracht 3
